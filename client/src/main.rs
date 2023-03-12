4987fdb10028585a9150222824238102119bef92
Will Cygan <32029716+wcygan@users.noreply.github.com>
2023-03-11 22:07:36 -0600

    echo server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut socket = TcpStream::connect("127.0.0.1:6655").await?;
+    socket.write_all(b"hello").await?;
+    let mut buf = [0; 1024];
+    let n = socket.read(&mut buf).await?;
+    println!("recv: {}", String::from_utf8_lossy(&buf[0..n]));
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6655").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            let n = socket.read(&mut buf).await.unwrap();
+            socket.write_all(&buf[0..n]).await.unwrap();
+        });
+    }
 }
