8d01831306d33eacc03353297d00521a33f5be41
Will Cygan <wcygan.io@gmail.com>
2023-03-17 20:17:23 -0500

    nit

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,39 +2,43 @@ use anyhow::Result;
 use common::signals::Signal;
 use common::signals::Signal::{Download, Upload};
 use common::transmission::Connection;
-use tokio::net::{TcpListener, ToSocketAddrs};
+use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = bind("0.0.0.0:6655").await?;
 
     loop {
-        let (socket, _) = listener.accept().await?;
-
+        let (stream, _) = listener.accept().await?;
         tokio::spawn(async move {
-            let mut conn = Connection::new(socket);
-            let action = conn.read::<Signal>().await?;
-
-            match action {
-                Some(Upload { filename }) => {
-                    conn.write(&Signal::Ack).await?;
-                    conn.read_bytes_to_file(filename.as_str()).await?;
-                }
-                Some(Download { filename }) => {
-                    conn.write(&Signal::Ack).await?;
-                    conn.send_bytes_from_file(filename.as_str()).await?;
-                }
-                None => {
-                    println!("Invalid signal");
-                }
-                _ => {
-                    println!("{:?} not handled", action)
-                }
-            }
-
+            handle(stream).await?;
             Ok::<_, anyhow::Error>(())
         });
     }
+}
+
+async fn handle(stream: TcpStream) -> Result<()> {
+    let mut conn = Connection::new(stream);
+    let action = conn.read::<Signal>().await?;
+
+    match action {
+        Some(Upload { filename }) => {
+            conn.write(&Signal::Ack).await?;
+            conn.read_bytes_to_file(filename.as_str()).await?;
+        }
+        Some(Download { filename }) => {
+            conn.write(&Signal::Ack).await?;
+            conn.send_bytes_from_file(filename.as_str()).await?;
+        }
+        None => {
+            println!("Invalid signal");
+        }
+        _ => {
+            println!("{:?} not handled", action)
+        }
+    }
+
+    Ok(())
 }
 
 async fn bind<A: ToSocketAddrs>(addr: A) -> Result<TcpListener> {
